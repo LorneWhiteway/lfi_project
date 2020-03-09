3f2b3eaa94d7e82e1e275f7320d679d547e5c645
LorneWhiteway <lorne.whiteway.13@ucl.ac.uk>
2020-03-09 16:49:06 +0000

    Ongoing development.

--- scripts/read_boxes.py
+++ scripts/read_boxes.py
@@ -18,6 +18,7 @@ def get_dark_type():
 
 # file_name will be something like "/share/splinter/ucapwhi/lfi_project/experiments/example.00100"
 # This is an edited version the code in the readtipsy.py file distributed with PKDGRAV3.
+# Returns an array with elements of type get_dark_type()
 def read_one_box(file_name):
 
     import numpy as np
@@ -27,28 +28,40 @@ def read_one_box(file_name):
         header = dict(zip(get_header_type().names, header[0]))
         return np.fromfile(in_file, dtype=get_dark_type(), count=header['Ndark']) # Just dark matter (not gas or stars)
     
-def main():
+
+# File_name will be something like "/share/splinter/ucapwhi/lfi_project/experiments/example.00100"
+def show_one_shell(file_name, shell_low, shell_high, nside):
     import numpy as np
     import healpy as hp
     import matplotlib.pyplot as plt
     
-    file_name = "/share/splinter/ucapwhi/lfi_project/experiments/example.00100"
     d = read_one_box(file_name)
     ra_list = []
     dec_list = []
-    for start_x in [-0.5, 0.5]:
-        for start_y in [-0.5, 0.5]:
-            for start_z in [-0.5, 0.5]:
-                dist = np.sqrt((d['x']-start_x)**2 + (d['y']-start_y)**2 + (d['z']-start_z)**2)
-                for (dd, xx, yy, zz) in zip(dist, -(start_x-d['x']), -(start_y-d['y']), -(start_z-d['z'])):
-                    if dd < 0.1137:
+    offsets = [-2.5, -1.5, -0.5, 0.5, 1.5, 2.5] # See my notes p. LFI24
+    index_dict = {}
+    for offset_x in offsets:
+        d_x = d['x']-offset_x
+        d_x_2 = d_x**2
+        for offset_y in offsets:
+            d_y = d['y']-offset_y
+            d_y_2 = d_y**2
+            for offset_z in offsets:
+                d_z = d['z']-offset_z
+                d_z_2 = d_z**2
+                dist = np.sqrt(d_x_2 + d_y_2 + d_z_2)
+                for (i, dd, xx, yy, zz) in zip(range(len(d_x)), dist, d_x, d_y, d_z):
+                
+                    if shell_low < dd and dd < shell_high:
                         ra_list.append(np.degrees(np.arctan2(yy, xx)))
                         dec_list.append(np.degrees(np.arcsin(zz/dd)))
+                        # Also register i so that we can see if it gets reused
+                        if i not in index_dict:
+                            index_dict[i] = 0
+                        index_dict[i] += 1
     ra = np.array(ra_list)
     dec = np.array(dec_list)
-    print(ra, dec)
     
-    nside = 16
     num_healpixels = hp.nside2npix(nside)
     map = np.zeros(num_healpixels)
     ids = hp.ang2pix(nside, ra, dec, False, lonlat=True)
@@ -57,8 +70,18 @@ def main():
     hp.mollview(map, title="", xsize=400, badcolor="grey")
     hp.graticule(dpar=30.0)
     plt.show()
+    
+    index_map = np.zeros(num_healpixels)
+    print(index_dict)
 
     
+def main():
+
+    file_name = "/share/splinter/ucapwhi/lfi_project/experiments/example.00099"
+    shell_low = 0.1137
+    shell_high = 0.228
+    nside = 16
+    show_one_shell(file_name, shell_low, shell_high, nside)
     
 
 
