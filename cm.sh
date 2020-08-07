77ab3f4ab7935c6e22a00b1826d738ac23d30c5f
Lorne Whiteway <lorne.whiteway@star.ucl.ac.uk>
2020-08-07 10:57:26 +0100

    Amended module files loaded on Wilkes so that we have a consistent set.

--- cm.sh
+++ cm.sh
@@ -11,7 +11,7 @@ fi
 echo "Build directory is " $BUILD_DIR
 echo "Proceed? (y/n)"
 
-read -n1 ans
+read ans
 
 if [[ $ans == "y" ]]; then
 	cd /rds/user/dc-whit2/rds-dirac-dp153/lfi_project
@@ -21,7 +21,6 @@ if [[ $ans == "y" ]]; then
 	rm -rf ./$BUILD_DIR
 	mkdir $BUILD_DIR
 	cd ./$BUILD_DIR
-	#cmake -DFFTW_ROOT=$FFTW_ROOT ..
-	cmake -DHDF5_ROOT=$HDF5_ROOT -DCMAKE_POLICY_DEFAULT_CMP0074=NEW ..
+	cmake ..
 	make
 fi
