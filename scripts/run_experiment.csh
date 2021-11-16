43782a89c4a33395b89b0b59214a1315695f8fc1
Lorne Whiteway <lorne.whiteway@star.ucl.ac.uk>
2021-11-16 15:50:43 +0000

    Removed various unneeded scripts.

--- scripts/run_experiment.csh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/tcsh
-cd $argv[1]
-/share/splinter/ucapwhi/lfi_project/pkdgrav3/build/pkdgrav3 ./control.par > ./example_output.txt
-
--- scripts/run_experiment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd $1
-/rds/user/dc-whit2/rds-dirac-dp153/lfi_project/pkdgrav3/build/pkdgrav3 ./control.par > ./example_output.txt
-
