c06517955256e2a90759edeffaab004ee8e11105
Lorne Whiteway <lorne.whiteway.13@ucl.ac.uk>
2022-01-24 10:35:45 +0000

    Added cm_tursa.sh and moved cm.sh to cm_wilkes.sh.

---
+++ cm_tursa.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Takes optional command-line argument to specify build directory name. Defaults to 'build'.
+
+if [[ $# == 0 ]]; then
+	BUILD_DIR="build"
+else
+	BUILD_DIR=$1
+fi
+
+echo "Build directory is " $BUILD_DIR
+echo "Proceed? (y/n)"
+
+read ans
+
+if [[ $ans == "y" ]]; then
+	cd /mnt/lustre/tursafs1/home/dp153/dp153/shared/lfi_project
+	source ./set_environment_tursa.sh BUILD
+	##git clone https://bitbucket.org/dpotter/pkdgrav3.git
+	cd ./pkdgrav3
+	rm -rf ./$BUILD_DIR
+	mkdir $BUILD_DIR
+	cd ./$BUILD_DIR
+	cmake ..
+	make
+fi
