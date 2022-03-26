abb65dff5f7074cabae1992d9510f7085f4e0887
Lorne Whiteway <lorne.whiteway.13@ucl.ac.uk>
2022-03-26 14:18:56 +0000

    Added files for runsL.

---
+++ runsL/hdf5/copy_template.sh
@@ -0,0 +1,1 @@
+cp /mnt/lustre/tursafs1/home/dp153/dp153/shared/lfi_project/runsL/hdf5/concept_outputs_250322_A/class_processed_batch6_{}.hdf5 /mnt/lustre/tursafs1/home/dp153/dp153/shared/lfi_project/runsL/run{}/
---
+++ runsL/start_template.sh
@@ -0,0 +1,2 @@
+cd /mnt/lustre/tursafs1/home/dp153/dp153/shared/lfi_project/runsL/run{}
+sbatch cuda_job_script_tursa
