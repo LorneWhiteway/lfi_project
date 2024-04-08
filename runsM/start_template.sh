9a6112d04df719c38e817e5b8def1595d8e0eb44
Lorne Whiteway <lorne.whiteway.13@ucl.ac.uk>
2024-04-08 15:52:55 +0100

    Start template for runsM needed to work for both Wilkes and Tursa.

--- runsM/start_template.sh
+++ runsM/start_template.sh
@@ -1,2 +1,4 @@
-cd /rds/user/dc-whit2/rds-dirac-dp153/lfi_project/runsM/run{}
-sbatch cuda_job_script_wilkes
+# cd /rds/user/dc-whit2/rds-dirac-dp153/lfi_project/runsM/run{}
+# sbatch cuda_job_script_wilkes
+cd /mnt/lustre/tursafs1/home/dp153/dp153/shared/lfi_project/runsM/run{}
+sbatch cuda_job_script_tursa
