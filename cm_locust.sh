2fd43d194ce58329103a085fb7ce4e3d1913a83f
ucapwhi <ucapwhi@locust.rc.ucl.ac.uk>
2024-09-27 11:18:36 +0100

    Start of work to add 'locust' machine.

--- cm_locust.sh
+++ cm_locust.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Takes optional command-line argument to specify build directory name. Defaults to 'build_tursa'.
+# Takes optional command-line argument to specify build directory name. Defaults to 'build'.
 
 if [[ $# == 0 ]]; then
-	BUILD_DIR="build_tursa"
+	BUILD_DIR="build"
 else
 	BUILD_DIR=$1
 fi
@@ -14,8 +14,8 @@ echo "Proceed? (y/n)"
 read ans
 
 if [[ $ans == "y" ]]; then
-	cd /mnt/lustre/tursafs1/home/dp327/dp327/shared/lfi_project
-	source ./set_environment_tursa.sh BUILD
+	cd /data/gower_st/shared/lfi_project
+	source ./set_environment_locust.sh BUILD
 	##git clone https://bitbucket.org/dpotter/pkdgrav3.git
 	cd ./pkdgrav3
 	rm -rf ./$BUILD_DIR
